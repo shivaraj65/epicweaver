e6521c8785a6b747dfe5473f0588ac90e9105fe9
shivaraj <shivaraj.feb@gmail.com>
2023-12-29 21:49:05 +0530

    dashboard flow + UI

--- src/containers/dashboard/index.tsx
+++ src/containers/dashboard/index.tsx
@@ -3,20 +3,90 @@ import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
-import styles from './dashboard.module.css';
+import styles from "./dashboard.module.css";
 
-import { Button, Row, Col, Input, message } from "antd";
+import { Button, Row, Col, Input, message, Layout, Divider } from "antd";
 import {
+  CompassOutlined,
+  FolderOpenOutlined,
+  ControlOutlined,
+  EditOutlined,
   UserOutlined,
-  EyeInvisibleOutlined,
-  EyeTwoTone,
-  CloseOutlined,
 } from "@ant-design/icons";
 
+const { Header, Content, Footer, Sider } = Layout;
+
 const Dashboard = () => {
+  const [selectedMenu, setSelectedMenu] = useState(0);
+
+  const menuItems: any = [
+    { name: "Explore", icon: <CompassOutlined /> },
+    { name: "Workspace", icon: <FolderOpenOutlined /> },
+    { name: "Settings", icon: <ControlOutlined /> },
+    { name: "Create story", icon: <EditOutlined /> },
+  ];
+
   return (
     <>
-      <span>dash cont</span>
+      <Layout className={styles.dashContainer}>
+        <Sider
+          theme="light"
+          //   color="#DAFFFB"
+          className={styles.sidebarContainer}
+          breakpoint="xxl"
+          collapsedWidth="0"
+          onBreakpoint={(broken: any) => {
+            console.log(broken);
+          }}
+          onCollapse={(collapsed: any, type: any) => {
+            console.log(collapsed, type);
+          }}
+        >
+          <div className={styles.spaceBtwn}>
+            <div>
+              <div className={styles.brandContainer}>
+                <h1 className={styles.brandText}>Epic Weaver</h1>
+              </div>
+              {menuItems.map((data: any, index: number) => {
+                return (
+                  <>
+                    <div
+                      key={index}
+                      className={
+                        selectedMenu === index
+                          ? styles.menuItemSelected
+                          : styles.menuItem
+                      }
+                      onClick={() => {
+                        setSelectedMenu(index);
+                      }}
+                    >
+                      {data?.icon}&nbsp;&nbsp;<p>{data?.name}</p>
+                    </div>
+                    <Divider className={styles.divider} />
+                  </>
+                );
+              })}
+            </div>
+            <div className={styles.profile}>
+              <UserOutlined />
+              &nbsp;&nbsp;user profile
+            </div>
+          </div>
+        </Sider>
+        <Layout>
+          <div
+            style={{
+              padding: 24,
+              minHeight: 360,
+              height: "100vh",
+              backgroundColor: "#efefef",
+            }}
+          >
+            content
+          </div>
+        </Layout>
+      </Layout>
     </>
   );
 };
