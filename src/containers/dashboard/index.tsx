32b6b0c42e181a1063c76ce487bc885563153b31
shivaraj <shivaraj.feb@gmail.com>
2023-12-30 19:41:37 +0530

    updated security

--- src/containers/dashboard/index.tsx
+++ src/containers/dashboard/index.tsx
@@ -41,74 +41,97 @@ const Dashboard = () => {
     { name: "Create story", icon: <EditOutlined /> },
   ];
 
+  const [credName, setCredName] = useState("");
+
+  const checkCredIdInLocalStorage = () => {
+    return localStorage.getItem("credId");
+  };
+
+  const checkCredNameInLocalStorage = () => {
+    return localStorage.getItem("credName");
+  };
+
+  useEffect(() => {
+    if (!checkCredIdInLocalStorage()) {
+      router.push("/");
+    } else {
+      let data: any = checkCredNameInLocalStorage()
+        ? checkCredNameInLocalStorage()
+        : "";
+      setCredName(data);
+    }
+  }, [checkCredIdInLocalStorage]);
+
   return (
     <>
-      <Layout className={styles.dashContainer}>
-        <Sider
-          theme="light"
-          className={styles.sidebarContainer}
-          breakpoint="xl"
-          collapsedWidth="0"
-          onBreakpoint={(broken: any) => {
-            // console.log(broken);
-          }}
-          onCollapse={(collapsed: any, type: any) => {
-            // console.log(collapsed, type);
-          }}
-        >
-          <div className={styles.spaceBtwn}>
-            <div>
-              <div className={styles.brandContainer}>
-                <h1 className={styles.brandText}>Epic Weaver</h1>
-              </div>
-              {menuItems.map((data: any, index: number) => {
-                return (
-                  <div key={index + 1}>
-                    <div
-                      className={
-                        selectedMenu === index
-                          ? styles.menuItemSelected
-                          : styles.menuItem
-                      }
-                      onClick={() => {
-                        if (index === 3) {
-                          setSelectedMenu(1);
-                          router.push("/dashboard");
-                        } else {
-                          setSelectedMenu(index);
-                        }
-                      }}
-                    >
-                      {data?.icon}&nbsp;&nbsp;<p>{data?.name}</p>
-                    </div>
-                    <Divider className={styles.divider} />
-                  </div>
-                );
-              })}
-            </div>
-            <div className={styles.profile}>
-              <UserOutlined />
-              &nbsp;&nbsp;&nbsp;
-              {credentials ? credentials.name + " uowegf  " : null}
-            </div>
-          </div>
-        </Sider>
-        <Layout>
-          <div
-            style={{
-              height: "100vh",
+      {credName && credName !== "" ? (
+        <Layout className={styles.dashContainer}>
+          <Sider
+            theme="light"
+            className={styles.sidebarContainer}
+            breakpoint="xl"
+            collapsedWidth="0"
+            onBreakpoint={(broken: any) => {
+              // console.log(broken);
+            }}
+            onCollapse={(collapsed: any, type: any) => {
+              // console.log(collapsed, type);
             }}
           >
-            {selectedMenu === 0 ? (
-              <Explore />
-            ) : selectedMenu === 1 ? (
-              <Workspace />
-            ) : selectedMenu === 2 ? (
-              <Settings />
-            ) : null}
-          </div>
+            <div className={styles.spaceBtwn}>
+              <div>
+                <div className={styles.brandContainer}>
+                  <h1 className={styles.brandText}>Epic Weaver</h1>
+                </div>
+                {menuItems.map((data: any, index: number) => {
+                  return (
+                    <div key={index + 1}>
+                      <div
+                        className={
+                          selectedMenu === index
+                            ? styles.menuItemSelected
+                            : styles.menuItem
+                        }
+                        onClick={() => {
+                          if (index === 3) {
+                            setSelectedMenu(1);
+                            router.push("/dashboard");
+                          } else {
+                            setSelectedMenu(index);
+                          }
+                        }}
+                      >
+                        {data?.icon}&nbsp;&nbsp;<p>{data?.name}</p>
+                      </div>
+                      <Divider className={styles.divider} />
+                    </div>
+                  );
+                })}
+              </div>
+              <div className={styles.profile}>
+                <UserOutlined />
+                &nbsp;&nbsp;&nbsp;
+                {credName}
+              </div>
+            </div>
+          </Sider>
+          <Layout>
+            <div
+              style={{
+                height: "100vh",
+              }}
+            >
+              {selectedMenu === 0 ? (
+                <Explore />
+              ) : selectedMenu === 1 ? (
+                <Workspace />
+              ) : selectedMenu === 2 ? (
+                <Settings />
+              ) : null}
+            </div>
+          </Layout>
         </Layout>
-      </Layout>
+      ) : null}
     </>
   );
 };
--- src/store/userCreds.js
+++ src/store/userCreds.js
@@ -1,14 +1,7 @@
 import { create } from "zustand";
 
+// depricated.. now used for app wide state management
 const userCreds = create((set) => ({
-  credentials: {
-    _id: "b6d83bc1-e3a8-42c5-a3da-4ca0c223db3c",
-    email: "shivaraj.feb@gmail.com",
-    name: "shivaraj",
-    password:
-      "d404559f602eab6fd602ac7680dacbfaadd13630335e951f097af3900e9de176b6db28512f2e000b9d04fba5133e8b1c6e8df59db3a8ab9d60be4b97cc9e81db",
-  },
-  setCredentials: (data) => set({ credentials: data }),
 
   selectedMenu: 1,
   setSelectedMenu: (data) => set({ selectedMenu: data }),
