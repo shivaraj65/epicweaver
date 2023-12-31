b43eb126ff9a1aa549c422619d0dfde6eaed80ac
shivaraj <shivaraj.feb@gmail.com>
2023-12-31 21:26:32 +0530

    schema changes

--- src/components/create/pageTwo/index.tsx
+++ src/components/create/pageTwo/index.tsx
@@ -1,11 +1,108 @@
 import { useState } from "react";
 import styles from "./pageTwo.module.css";
 
-const PageTwo = () => {
+import { Breadcrumb, Input } from "antd";
+import { SendOutlined } from "@ant-design/icons";
+
+interface Props {
+  setPageFlag: Function;
+}
+
+const PageTwo: React.FC<Props> = (props) => {
+  const [input, setInput] = useState("");
+
+  const promptSubmit = () => {
+    console.log("prompt submit", input);
+  };
+
+  const StoryData = [
+    {
+      id: "1",
+      context: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      minifiedContext: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      previousNodeId: null,
+      createdAt: "some date",
+      userId: 1234,
+      publishedStatus: null,
+      prompt:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+    {
+      id: "2",
+      context: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      minifiedContext: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      previousNodeId: "1",
+      createdAt: "2023-01-01T12:00:00Z", 
+      userId: 1234,
+      publishedStatus: null,
+      prompt:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+    {
+      id: "3",
+      context: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      minifiedContext: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      previousNodeId: "1",
+      createdAt: "2023-01-02T12:00:00Z", 
+      userId: 1234,
+      publishedStatus: null,
+      prompt:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+    {
+      id: "4",
+      context: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      minifiedContext: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      previousNodeId: "1",
+      createdAt: "2023-01-03T12:00:00Z", 
+      userId: 1234,
+      publishedStatus: null,
+      prompt:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+    {
+      id: "5",
+      context: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      minifiedContext: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      previousNodeId: "2",
+      createdAt: "2023-01-04T12:00:00Z", 
+      userId: 1234,
+      publishedStatus: null,
+      prompt:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+  ];
+  
   return (
-    <div>
-      <p>pageTwo</p>
-    </div>
+    <>
+      <Breadcrumb style={{ marginBottom: "6px" }}>
+        <Breadcrumb.Item
+          onClick={() => {
+            props.setPageFlag(0);
+          }}
+          className={styles.breadcrumbItem}
+        >
+          Workspace
+        </Breadcrumb.Item>
+        <Breadcrumb.Item className={styles.breadcrumbItem}>
+          Create Story
+        </Breadcrumb.Item>
+      </Breadcrumb>
+      <div className={styles.container}>
+        container p2
+        <div className={styles.floater}>
+          <Input
+            className={styles.inputFloater}
+            autoFocus
+            size="large"
+            suffix={<SendOutlined />}
+            value={input}
+            onChange={(e) => {
+              setInput(e.target.value);
+            }}
+            onPressEnter={() => {
+              promptSubmit();
+            }}
+            placeholder="Enter your prompt here"
+          />
+        </div>
+      </div>
+    </>
   );
 };
 
