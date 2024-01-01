4f43b1f5ff3c63c655cc3dfc5db3164fb521a1b4
shivaraj <shivaraj.feb@gmail.com>
2024-01-01 13:30:24 +0530

    tree traverse logic

--- src/components/create/pageTwo/index.tsx
+++ src/components/create/pageTwo/index.tsx
@@ -3,7 +3,12 @@ import styles from "./pageTwo.module.css";
 import Image from "next/image";
 
 import { Breadcrumb, Input } from "antd";
-import { SendOutlined, UserOutlined } from "@ant-design/icons";
+import {
+  SendOutlined,
+  UserOutlined,
+  LeftOutlined,
+  RightOutlined,
+} from "@ant-design/icons";
 
 import PALM from "@/assets/icons/PALM.svg";
 import GPT from "@/assets/icons/GPT.svg";
@@ -19,7 +24,7 @@ const rawData = [
     id: "1",
     context:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed scelerisque magna vel aliquet placerat. Praesent accumsan rhoncus enim, sit amet porttitor turpis convallis et. Nulla ullamcorper dapibus finibus. Nulla sit amet porta turpis, sit amet luctus leo. Aliquam nec placerat arcu. Fusce rutrum rutrum viverra. Donec eu lectus a sem venenatis aliquet. Duis fermentum lorem a dui blandit pretium. Morbi iaculis, enim vehicula dapibus porta, ligula lacus finibus lacus, eu dignissim leo dolor sed libero. Donec aliquet bibendum libero, a aliquam est. Nam varius erat eu sem sagittis, in placerat urna vulputate.",
-    minifiedContext: "Lorem ipsum ",
+    minifiedContext: "root story ",
     previousNodeId: null,
     createdAt: "some date",
     userId: 1234,
@@ -30,7 +35,7 @@ const rawData = [
     id: "2",
     context:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed scelerisque magna vel aliquet placerat. Praesent accumsan rhoncus enim, sit amet porttitor turpis convallis et. Nulla ullamcorper dapibus finibus. Nulla sit amet porta turpis, sit amet luctus leo. Aliquam nec placerat arcu. Fusce rutrum rutrum viverra. Donec eu lectus a sem venenatis aliquet. Duis fermentum lorem a dui blandit pretium. Morbi iaculis, enim vehicula dapibus porta, ligula lacus finibus lacus, eu dignissim leo dolor sed libero. Donec aliquet bibendum libero, a aliquam est. Nam varius erat eu sem sagittis, in placerat urna vulputate.",
-    minifiedContext: "Lorem ipsum ",
+    minifiedContext: "twist 1 ",
     previousNodeId: "1",
     createdAt: "2023-01-01T12:00:00Z",
     userId: 1234,
@@ -41,7 +46,7 @@ const rawData = [
     id: "3",
     context:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed scelerisque magna vel aliquet placerat. Praesent accumsan rhoncus enim, sit amet porttitor turpis convallis et. Nulla ullamcorper dapibus finibus. Nulla sit amet porta turpis, sit amet luctus leo. Aliquam nec placerat arcu. Fusce rutrum rutrum viverra. Donec eu lectus a sem venenatis aliquet. Duis fermentum lorem a dui blandit pretium. Morbi iaculis, enim vehicula dapibus porta, ligula lacus finibus lacus, eu dignissim leo dolor sed libero. Donec aliquet bibendum libero, a aliquam est. Nam varius erat eu sem sagittis, in placerat urna vulputate.",
-    minifiedContext: "Lorem ipsum ",
+    minifiedContext: "twist 2 ",
     previousNodeId: "1",
     createdAt: "2023-01-02T12:00:00Z",
     userId: 1234,
@@ -52,7 +57,7 @@ const rawData = [
     id: "4",
     context:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed scelerisque magna vel aliquet placerat. Praesent accumsan rhoncus enim, sit amet porttitor turpis convallis et. Nulla ullamcorper dapibus finibus. Nulla sit amet porta turpis, sit amet luctus leo. Aliquam nec placerat arcu. Fusce rutrum rutrum viverra. Donec eu lectus a sem venenatis aliquet. Duis fermentum lorem a dui blandit pretium. Morbi iaculis, enim vehicula dapibus porta, ligula lacus finibus lacus, eu dignissim leo dolor sed libero. Donec aliquet bibendum libero, a aliquam est. Nam varius erat eu sem sagittis, in placerat urna vulputate.",
-    minifiedContext: "Lorem ipsum ",
+    minifiedContext: "twist 3 ",
     previousNodeId: "1",
     createdAt: "2023-01-03T12:00:00Z",
     userId: 1234,
@@ -63,7 +68,7 @@ const rawData = [
     id: "5",
     context:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed scelerisque magna vel aliquet placerat. Praesent accumsan rhoncus enim, sit amet porttitor turpis convallis et. Nulla ullamcorper dapibus finibus. Nulla sit amet porta turpis, sit amet luctus leo. Aliquam nec placerat arcu. Fusce rutrum rutrum viverra. Donec eu lectus a sem venenatis aliquet. Duis fermentum lorem a dui blandit pretium. Morbi iaculis, enim vehicula dapibus porta, ligula lacus finibus lacus, eu dignissim leo dolor sed libero. Donec aliquet bibendum libero, a aliquam est. Nam varius erat eu sem sagittis, in placerat urna vulputate.",
-    minifiedContext: "Lorem ipsum ",
+    minifiedContext: "twist 21 ",
     previousNodeId: "2",
     createdAt: "2023-01-04T12:00:00Z",
     userId: 1234,
@@ -201,6 +206,70 @@ const PageTwo: React.FC<Props> = (props) => {
     console.log("prompt submit", input);
   };
 
+  const controllsCheckerLeft = (data: any) => {
+    if (data.previousNodeId !== null) {
+      let list = silverLayer[data.previousNodeId];
+      if (list.length <= 1 || data.id === list[0].id) {
+        return false;
+      } else {
+        return true;
+      }
+    } else {
+      return false;
+    }
+  };
+
+  const controllsCheckerRight = (data: any) => {
+    if (data.previousNodeId !== null) {
+      let list = silverLayer[data.previousNodeId];
+      if (list.length <= 1 || data.id === list[list.length - 1].id) {
+        return false;
+      } else {
+        return true;
+      }
+    } else {
+      return false;
+    }
+  };
+
+  const treeController = (data: any, method: string) => {
+    let newGoldLayer = [];
+    for (let i = 0; i < goldLayer.length; i++) {
+      if (goldLayer[i].id === data.id) {
+        break;
+      }
+      newGoldLayer.push(goldLayer[i]);
+    }
+    let traverseArr = silverLayer[data.previousNodeId];
+    if (method === "left") {
+      let i = 1;
+      for (i = 1; i < traverseArr.length; i++) {
+        if (traverseArr[i].id === data.id) {
+          break;
+        }
+      }
+      let ticker = [traverseArr[i - 1]];
+      while (ticker) {
+        newGoldLayer.push(ticker[0]);
+        ticker = silverLayer[ticker[0].id];
+      }
+      setGoldlayer(newGoldLayer);
+    } else {
+      let i = 0;
+      for (i = 0; i < traverseArr.length - 1; i++) {
+        if (traverseArr[i].id === data.id) {
+          break;
+        }
+      }
+      let ticker = [traverseArr[i + 1]];
+      while (ticker) {
+        newGoldLayer.push(ticker[0]);
+        ticker = silverLayer[ticker[0].id];
+      }
+      setGoldlayer(newGoldLayer);
+    }
+  };
+
   return (
     <>
       <Breadcrumb style={{ marginBottom: "6px" }}>
@@ -222,32 +291,62 @@ const PageTwo: React.FC<Props> = (props) => {
           {goldLayer &&
             goldLayer.map((data: any, index: number) => {
               return (
-                <div key={index} className={styles.chatContainer}>
-                  <div className={styles.promptDiv}>
-                    <p className={styles.promptText}>{data.prompt}</p>
-                    <UserOutlined className={styles.userIcon} />
-                  </div>
+                <div
+                  key={index}
+                  className={
+                    !controllsCheckerLeft(data) && !controllsCheckerRight(data)
+                      ? styles.chatContainerLRMargin
+                      : !controllsCheckerLeft(data)
+                      ? styles.chatContainerLeftMargin
+                      : !controllsCheckerRight(data)
+                      ? styles.chatContainerRightMargin
+                      : styles.chatContainer
+                  }
+                >
+                  {controllsCheckerLeft(data) ? (
+                    <LeftOutlined
+                      className={styles.controls}
+                      onClick={() => {
+                        treeController(data, "left");
+                      }}
+                    />
+                  ) : null}
 
-                  <div className={styles.contextDiv}>
-                    <Image
-                      src={PALM}
-                      width={25}
-                      height={25}
-                      alt={"icon"}
-                    ></Image>
-                    <div className={styles.contextInnerDiv}>
-                      {data.previousNodeId !== null ? (
-                        <p className={styles.title}>
-                          <span className={styles.titleTitle}>
-                            Title : &nbsp;
-                          </span>
-                          { data.minifiedContext}
-                        </p>
-                      ) : null}
+                  <div>
+                    <div className={styles.promptDiv}>
+                      <p className={styles.promptText}>{data.prompt}</p>
+                      <UserOutlined className={styles.userIcon} />
+                    </div>
 
-                      <p className={styles.storyBlock}>{data.context}</p>
+                    <div className={styles.contextDiv}>
+                      <Image
+                        src={PALM}
+                        width={25}
+                        height={25}
+                        alt={"icon"}
+                      ></Image>
+                      <div className={styles.contextInnerDiv}>
+                        {data.previousNodeId !== null ? (
+                          <p className={styles.title}>
+                            <span className={styles.titleTitle}>
+                              Title : &nbsp;
+                            </span>
+                            {data.minifiedContext}
+                          </p>
+                        ) : null}
+
+                        <p className={styles.storyBlock}>{data.context}</p>
+                      </div>
                     </div>
                   </div>
+                  {controllsCheckerRight(data) ? (
+                    <RightOutlined
+                      className={styles.controls}
+                      onClick={() => {
+                        treeController(data, "right");
+                      }}
+                    />
+                  ) : null}
                 </div>
               );
             })}
